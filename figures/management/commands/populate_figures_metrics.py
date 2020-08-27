d3c43163415070b304fee92762ac6c566db30813
rehan99000 <rehan99000@gmail.com>
2020-08-27 12:37:07 +0500

    removed unnecessary import

--- figures/management/commands/populate_figures_metrics.py
+++ figures/management/commands/populate_figures_metrics.py
@@ -11,7 +11,6 @@ from __future__ import print_function
 
 from __future__ import absolute_import
 from textwrap import dedent
-from datetime import datetime
 import warnings
 
 from django.core.management import call_command
