ff1617825dd446c08bf9403471f4641ea6acb377
Asad Manzoor <asad33manzoor@gmail.com>
2020-09-04 17:54:10 +0500

    Page wise Pagination Updated for Learner Details API (#11)

--- figures/pagination.py
+++ figures/pagination.py
@@ -1,23 +1,29 @@
-'''Paginatiors for Figures
-
-'''
+"""
+Paginators for Figures
+"""
 
 from __future__ import absolute_import
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 
 
-class FiguresTopStatsPagination(LimitOffsetPagination):
-    '''Custom Figures paginator to make the number of records returned consistent
-    '''
-    default_limit = 10
+class FiguresTopStatsPagination(PageNumberPagination):
+    """
+    Custom Figures paginator to make the number of records returned consistent
+    """
+    page_size = 10
+    page_size_query_param = 'page'
+    max_page_size = 1000
+
 
 class FiguresLimitOffsetPagination(LimitOffsetPagination):
-    '''Custom Figures paginator to make the number of records returned consistent
-    '''
+    """
+    Custom Figures paginator to make the number of records returned consistent
+    """
     default_limit = 20
 
 
 class FiguresKiloPagination(LimitOffsetPagination):
-    '''Custom Figures paginator to make the number of records returned consistent
-    '''
+    """
+    Custom Figures paginator to make the number of records returned consistent
+    """
     default_limit = 1000
