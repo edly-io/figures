6e99baacb3c81b2725303a5ad5929d9571f7fcf0
Asad Manzoor <asad33manzoor@gmail.com>
2020-09-10 15:52:35 +0500

    Add Current Page No and Total Pages Count to Pagination Response (#12)

--- figures/pagination.py
+++ figures/pagination.py
@@ -3,15 +3,22 @@ Paginators for Figures
 """
 
 from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.response import Response
 
 
 class FiguresTopStatsPagination(PageNumberPagination):
     """
     Custom Figures paginator to make the number of records returned consistent
     """
-    page_size = 10
-    page_size_query_param = 'page'
-    max_page_size = 1000
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+            'total_pages': self.page.paginator.num_pages
+        })
 
 
 class FiguresLimitOffsetPagination(LimitOffsetPagination):
