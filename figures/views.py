5e975554d7200213b0d5d212face95d95e15f36f
Taimoor Ahmed <taimoor.ahmed@arbisoft.com>
2022-06-01 12:59:40 +0500

    changes

--- figures/views.py
+++ figures/views.py
@@ -533,7 +533,7 @@ class LearnerDetailsPDFViewSet(CommonAuthMixin, viewsets.ReadOnlyModelViewSet):
         platform_name = current_site_configuration.get_value('PLATFORM_NAME', settings.PLATFORM_NAME)
         from_address =  current_site_configuration.get_value('email_from_address', settings.DEFAULT_FROM_EMAIL)
         logo_url = current_site_configuration.get_value('BRANDING', {}).get('logo', '')
-        self.send_learners_data_pdf.delay(
+        self.send_learners_data_pdf(
             users=list(queryset),
             email=request.user.email,
             site_id=site.id,
@@ -1162,6 +1162,7 @@ class SiteViewSet(StaffUserOnDefaultSiteAuthMixin, viewsets.ReadOnlyModelViewSet
     Access is restricted to global (Django instance) staff
     """
     model = Site
+
     queryset = Site.objects.all()
     pagination_class = FiguresLimitOffsetPagination
     serializer_class = SiteSerializer
